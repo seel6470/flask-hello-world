40f92acdbf6757ddec4fe6276766f827b8e9f456
Seth Ely <seel6470@colorado.edu>
2024-11-01 16:26:22 +0000

    Added db_create route to setup basketball placeholder data

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 import psycopg2
-
+import prefix
 app = Flask(__name__)
+prefix.use_PrefixMiddleware(app)
 
 @app.route('/')
 def hello_world():
@@ -12,3 +13,27 @@ def db_test():
     conn = psycopg2.connect("postgresql://lab_10_test_db_user:tQ7u6YUYctCEThZ8765T9Bq38FDzJiI8@dpg-csifge1u0jms73fbcag0-a/lab_10_test_db")
     conn.close()
     return "Test DB connection successful!"
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("your_db_url_here")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+        );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Created"
+
+
+# main driver function
+if __name__ == '__main__':
+    # run() method of Flask class runs the application 
+    # on the local development server using port 3308 instead of port 5000.
+    app.run(host='0.0.0.0', port=3308)
